e42f2514fdea3991a90f8f2925849a5871c71604
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-03-20 06:30:14 +0300

    get api secret from env var

--- functions/generate_signature.py
+++ functions/generate_signature.py
@@ -5,7 +5,7 @@ import os
 
 def generate_signature(timestamp, method, endpoint, body):
     prehash_string = timestamp + method + endpoint + body
-    # os.getenv("API_SECRET")
-    hashed_key = hmac.new(bytes("API_SECRET", 'UTF-8'), msg=bytes(prehash_string, 'UTF-8'), digestmod=hashlib.sha256).digest()
+    api_secret = os.getenv("API_SECRET")
+    hashed_key = hmac.new(bytes(api_secret, 'UTF-8'), msg=bytes(prehash_string, 'UTF-8'), digestmod=hashlib.sha256).digest()
     signature = base64.b64encode(hashed_key).decode()
     return signature
