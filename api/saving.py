a6e49728cc60af305c124b7edc712f93c48e701e
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-03-25 09:05:23 +0300

    added default action on saving

--- api/saving.py
+++ api/saving.py
@@ -1,7 +1,10 @@
 from ..functions.api_request import api_request
 
-async def create_saving(amount: str, deposit: str):
-  api_response = await api_request("POST", "/saving/create", "", {"amount": amount, "deposit": deposit})
+async def create_saving(amount: str, action: str):
+  payload = {"amount": amount, "action": "deposit"}
+  if(action != None):
+    payload = {"amount": amount, "action": action}
+  api_response = await api_request("POST", "/saving/create", "", {"amount": amount, "action": action})
   return api_response
 
 async def withdraw_saving():
