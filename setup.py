2c0dbeb7ddedf3c290eec0dd0e3c35fe258ed0ac
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-04-04 16:35:51 +0300

    add long description

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version='0.1.12',
     author="Ermiyas Bitew",
     author_email="ermiyasbitew239@gmail.com",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages()
 )
