f1860f9867db4c55a25ad7e0199207852f5d53c9
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-04-04 14:37:34 +0300

    version number updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='0.1.10',
+    version='0.1.11',
     author="Ermiyas Bitew",
     author_email="ermiyasbitew239@gmail.com",
     packages=find_packages()
