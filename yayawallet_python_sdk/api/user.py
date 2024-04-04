5e792212274c08fed911d0cee4ddf2f319009763
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-04-04 11:27:57 +0300

    added search user functionality

--- yayawallet_python_sdk/api/user.py
+++ yayawallet_python_sdk/api/user.py
@@ -7,3 +7,7 @@ async def get_organization():
 async def get_profile():
   api_response = await api_request("GET", "/user/profile", "", None)
   return api_response
+
+async def search_user(query: str):
+  api_response = await api_request("POST", "/user/search", "", { "query": query })
+  return api_response
