f23f6c4829836e204da8f6f6c2d845c5fc82adbd
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-07-16 08:51:40 +0300

    restructured kyc endpoints

--- yayawallet_python_sdk/api/kyc.py
+++ yayawallet_python_sdk/api/kyc.py
@@ -1,9 +1,17 @@
 from ..functions.api_request import api_request
 
-async def get_licenses_by_tin(tin):    
+async def request_otp(fin):    
+  api_response = await api_request("GET", "/kyc/fayda/request-otp/" + fin, "", None)
+  return api_response
+
+async def get_kyc_details(fin, transaction_id, otp):    
+  api_response = await api_request("GET", "/kyc/fayda/get-kyc-details/" + fin + "/" + transaction_id + "/" + otp, "", None)
+  return api_response
+
+async def find_by_tin(tin):    
   api_response = await api_request("GET", "/kyc/etrade/find-by-tin/" + tin, "", None)
   return api_response
 
-async def get_business_details(tin: str, licenseNumber: str):
+async def find_by_license_number(tin: str, licenseNumber: str):
   api_response = await api_request("POST", "/kyc/etrade/find-by-license-number/" + tin, "", {"licenseNumber": licenseNumber})
   return api_response
