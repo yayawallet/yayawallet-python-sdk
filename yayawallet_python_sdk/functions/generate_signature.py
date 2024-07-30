0212125036c41d361dd2a030b46c6a43499f62eb
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-07-30 11:54:16 +0300

    parse api key name to underscore

--- yayawallet_python_sdk/functions/generate_signature.py
+++ yayawallet_python_sdk/functions/generate_signature.py
@@ -2,13 +2,16 @@ import hashlib
 import hmac
 import base64
 import os
+from django.http import StreamingHttpResponse
 
 def generate_signature(timestamp, method, endpoint, body, api_key):
     prehash_string = timestamp + method + endpoint + body
     if api_key is None:
         api_secret = os.getenv("YAYA_API_SECRET")
     else:
-        api_secret = os.getenv(api_key + "_YAYA_API_SECRET")
+        dynamic_name=api_key + "_YAYA_API_SECRET"
+        parsed_name=dynamic_name.replace('-', '_')
+        api_secret = os.getenv(parsed_name)
     hashed_key = hmac.new(bytes(api_secret, 'UTF-8'), msg=bytes(prehash_string, 'UTF-8'), digestmod=hashlib.sha256).digest()
     signature = base64.b64encode(hashed_key).decode()
     return signature
