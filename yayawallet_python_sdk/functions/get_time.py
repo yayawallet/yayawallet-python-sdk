4a02c9b52fef95c04b99f12f1e09b4a86797f9e4
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-08-03 08:23:43 +0300

    capitalize api key

--- yayawallet_python_sdk/functions/get_time.py
+++ yayawallet_python_sdk/functions/get_time.py
@@ -5,7 +5,7 @@ import json
 async def get_time():
     url = os.getenv("YAYA_API_URL") + '/time'
     try:
-        response = httpx.get(url)
+        response = httpx.get(url, timeout=100.0)
         response.raise_for_status()
         return json.loads(response.text)
     except httpx.HTTPError as exc:
