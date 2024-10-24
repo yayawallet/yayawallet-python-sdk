a055c1671e5e40cb800fbdb428ce49df06ca5f3f
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-10-24 21:45:15 +0300

    added update user method

--- yayawallet_python_sdk/api/user.py
+++ yayawallet_python_sdk/api/user.py
@@ -8,6 +8,8 @@ from ..serializers.user_serializers import CustomerUserSerializer, BusinessUserS
 
 import json
 
+from typing import Optional
+
 async def get_organization(api_key = None):
   api_response = await api_request("GET", "/user/organization", "", None, api_key)
   return api_response
@@ -63,3 +65,33 @@ async def create_business_user(data, api_key = None):
         content_type='application/json',
         status=status.HTTP_400_BAD_REQUEST,
       )
+  
+async def update_user(
+      account_name: Optional[str] = None,
+      name: Optional[str] = None,
+      gender: Optional[str] = None, 
+      region: Optional[str] = None, 
+      location: Optional[str] = None, 
+      date_of_birth: Optional[str] = None, 
+      sms_notification_enable: Optional[bool] = None, 
+      email_notification_enable: Optional[bool] = None, 
+      app_notification_enable: Optional[bool] = None, 
+      api_key: str = None
+  ):
+  payload = {
+    **{
+      key: value for key, value in {
+        "account_name": account_name, 
+        "name": name, 
+        "gender": gender, 
+        "region": region, 
+        "location": location, 
+        "date_of_birth": date_of_birth, 
+        "sms_notification_enable": sms_notification_enable, 
+        "email_notification_enable": email_notification_enable, 
+        "app_notification_enable": app_notification_enable
+      }.items() if value is not None
+    }
+  }
+  api_response = await api_request("POST", "/user/update", "", payload, api_key)
+  return api_response
