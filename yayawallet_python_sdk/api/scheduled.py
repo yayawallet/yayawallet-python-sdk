dad949d66d07aebc4457e65cd8e7f7c2dc146e94
suraffy <surafelay@gmail.com>
2025-01-20 13:27:45 +0300

    Add institution_code parameter to create scheduled payment function

--- yayawallet_python_sdk/api/scheduled.py
+++ yayawallet_python_sdk/api/scheduled.py
@@ -1,8 +1,8 @@
 from ..functions.api_request import api_request
 from typing import Optional
 
-async def create(account_number: str, amount: str, reason: str, recurring: str, start_at: str, meta_data, api_key: str = None):
-  api_response = await api_request("POST", "/scheduled-payment/create", "", {"account_number": account_number, "amount": amount, "reason": reason, "recurring": recurring, "start_at": start_at, "meta_data": meta_data}, api_key)
+async def create(institution_code: str, account_number: str, amount: str, reason: str, recurring: str, start_at: str, meta_data, api_key: str = None):
+  api_response = await api_request("POST", "/scheduled-payment/create", "", {"institution_code": institution_code, "account_number": account_number, "amount": amount, "reason": reason, "recurring": recurring, "start_at": start_at, "meta_data": meta_data}, api_key)
   return api_response
 
 async def get_list(param: Optional[dict] = None, api_key: Optional[str] = None):
