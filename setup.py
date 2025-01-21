572123d75d8a2e9f5ec7a3d42eaefeb467258add
suraffy <surafelay@gmail.com>
2025-01-21 15:02:48 +0300

    Bump version to 0.4.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='0.4.0',
+    version='0.4.1',
     author="YaYa Wallet",
     author_email="contact@yayawallet.com",
     long_description=open('README.md').read(),
