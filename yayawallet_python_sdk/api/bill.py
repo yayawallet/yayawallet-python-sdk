bc386500deb35dffa371c422ad47d6924419df40
Birhan Tamiru <birhantamiru281@gmail.com>
2025-02-24 09:58:43 +0300

    feature/created-utilities-list-endpoint-for-bill-payment

--- yayawallet_python_sdk/api/bill.py
+++ yayawallet_python_sdk/api/bill.py
@@ -102,3 +102,38 @@ async def bulk_bill_list(client_yaya_account, param: Optional[dict] = None, api_
 async def bulk_bill_find(client_yaya_account, bill_id, api_key = None):
   api_response = await api_request("POST", "/bill/find", "", {"bill_id": bill_id, "client_yaya_account": client_yaya_account}, api_key)
   return api_response
+
+
+async def get_utilities_list(api_key: Optional[str] = None):
+    """
+    Fetches the list of utilities.
+
+    :param api_key: Optional API key for authentication.
+    :return: API response.
+    """
+    api_response = await api_request("GET", "/utilities/list", "", None, api_key)
+    return api_response
+
+async def post_utilities_list(
+    bill_id: str,
+    client_yaya_account: str,
+    api_key: Optional[str] = None
+):
+    """
+    Posts a request to fetch utilities list based on bill ID and client Yaya account.
+
+    :param bill_id: The ID of the bill.
+    :param client_yaya_account: The Yaya account of the bill provider.
+    :param api_key: Optional API key for authentication.
+    :return: API response.
+    """
+    payload = {
+        "bill_id": bill_id,
+        "client_yaya_account": client_yaya_account
+    }
+    
+    api_response = await api_request("POST", "/utilities/list", "", payload, api_key)
+    return api_response
+
+
+
