3f6b8c40f69a306edcdcbc836061c1a23f3f48e4
Birhan Tamiru <birhantamiru281@gmail.com>
2025-02-26 11:48:45 +0300

    update-sdk-version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='0.5.1',
+    version='0.5.2',
     author="YaYa Wallet",
     author_email="contact@yayawallet.com",
     long_description=open('README.md').read(),
