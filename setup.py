c358075891ec209de300194e1d3e5aeceea5ec2b
suraffy <surafelay@gmail.com>
2025-07-21 15:05:47 +0300

    update version to 1.2.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='1.2.1',
+    version='1.2.2',
     author="YaYa Wallet",
     author_email="contact@yayawallet.com",
     long_description=open('README.md').read(),
