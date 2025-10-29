d229d0a3c0883dc2458bf2bdcd6c948c472f440a
Surafel Araya <surafelay@gmail.com>
2025-10-29 11:02:44 +0300

    feat: add cash payment related apis

---
+++ yayawallet_python_sdk/api/cash_payment.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from ..functions.api_request import api_request
+
+async def pay_by_cash(urlSlag: str, receiver: str, notes: Optional[str] = None, api_key: str = None):
+  api_response = await api_request("POST", f"/payment-intent/{urlSlag}", "", {"receiver": receiver, "notes":  notes}, api_key)
+  return api_response
+
+async def get_daily_cash_transactions(accountId: str, date: str , api_key: str = None):
+  api_response = await api_request("GET", f"/payment-intent/cash-transactions/daily/{accountId}/{date}", "", None, api_key)
+  return api_response
+
+async def get_blocked_balace(accountId: str, api_key: str = None):
+  api_response = await api_request("GET", f"/cash-settlement/blocked-balances/{accountId}", "", None, api_key)
+  return api_response
+
+async def process_settlement(data: dict, api_key: str = None):
+  api_response = await api_request("POST", "/cash-settlement/process", "", data, api_key)
+  return api_response
+
+async def get_setlement_statics(accountId: str, api_key: str = None):
+  api_response = await api_request("GET", f"/cash-settlement/stats/{accountId}", "", None, api_key)
+  return api_response
