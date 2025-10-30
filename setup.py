7d92b1af8a82a0cbf87758ce89a7d0a8e127fc11
Surafel Araya <69146977+suraffy@users.noreply.github.com>
2025-10-31 00:03:16 +0300

    Bump version to 1.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='1.8.0',
+    version='1.8.1',
     author="YaYa Wallet",
     author_email="contact@yayawallet.com",
     long_description=open('README.md').read(),
