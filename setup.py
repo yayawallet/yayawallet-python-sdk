3aed772e37b7776d06d5cb219646b292fa031178
Surafel Araya <surafelay@gmail.com>
2025-11-04 11:59:37 +0300

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yayawallet-python-sdk',
-    version='1.9.0',
+    version='1.9.2',
     author="YaYa Wallet",
     author_email="contact@yayawallet.com",
     long_description=open('README.md').read(),
