9297413db994b6df6b8df5a7e895fb80421bb5e4
Surafel Araya <surafelay@gmail.com>
2025-11-05 11:17:19 +0300

    fix: remove accountId

--- yayawallet_python_sdk/api/cash_payment.py
+++ yayawallet_python_sdk/api/cash_payment.py
@@ -5,8 +5,8 @@ async def pay_by_cash(urlSlag: str, receiver: str, notes: Optional[str] = None, 
   api_response = await api_request("POST", f"/payment-intent/{urlSlag}/mark-cash-paid", "", {"receiver": receiver, "notes":  notes}, api_key)
   return api_response
 
-async def get_daily_cash_transactions(accountId: str, date: str , api_key: str = None):
-  api_response = await api_request("GET", f"/payment-intent/cash-transactions/daily/{accountId}/{date}", "", None, api_key)
+async def get_daily_cash_transactions(date: str , api_key: str = None):
+  api_response = await api_request("GET", f"/payment-intent/cash-transactions/daily/{date}", "", None, api_key)
   return api_response
 
 async def get_blocked_balace(api_key: str = None):
@@ -17,8 +17,8 @@ async def process_settlement(data: dict, api_key: str = None):
   api_response = await api_request("POST", "/cash-settlement/process", "", data, api_key)
   return api_response
 
-async def get_setlement_statics(accountId: str, api_key: str = None):
-  api_response = await api_request("GET", f"/cash-settlement/stats/{accountId}", "", None, api_key)
+async def get_setlement_statics(api_key: str = None):
+  api_response = await api_request("GET", "/cash-settlement/stats", "", None, api_key)
   return api_response
 
 async def send_otp_payment(phone: str, uniqueRef: str, api_key: str = None):
